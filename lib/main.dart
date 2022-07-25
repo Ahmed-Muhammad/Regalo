c66a0437cf4081e16280043a1a7d0916d07ccea6
Ahmad M. Hassanien <44645999+Ahmed-Muhammad@users.noreply.github.com>
2022-07-25 19:31:57 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:mercado/src/core/themes/app_theme.dart';
+
+import 'src/core/bloc_observer/bloc_observer.dart';
+import 'src/features/splash_screen/screens/splash_screen.dart';
+
+void main() {
+  //Bloc Observer
+  BlocOverrides.runZoned(
+    () => runApp(const MercadoApp()),
+    blocObserver: MyBlocObserver(),
+  );
+  runApp(const MercadoApp());
+}
+
+class MercadoApp extends StatelessWidget {
+  const MercadoApp({Key? key}) : super(key: key);
+
+//ss
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: const Scaffold(
+        body: SplashScreen(),
+      ),
+    );
+  }
+}
